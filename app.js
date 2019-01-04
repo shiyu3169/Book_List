1b71c0bf5bafdbcb1ea9c568e1727b86f8c2dab3
Shiyu Wang <shiyu3169@gmail.com>
2019-01-04 13:34:42 -0500

    display books

--- app.js
+++ app.js
@@ -8,10 +8,46 @@ class Book {
 }
 
 // UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "3434434"
+      },
+      {
+        title: "Book Two",
+        author: "Jane Doe",
+        isbn: "45545"
+      }
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach(book => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="# class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
 
 // Store Class: Handle Storage
 
 // Event: Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: Add a Book
 
