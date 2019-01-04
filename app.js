4921e79afa95b3482e3dc5aeb35d613e368655fe
Shiyu Wang <shiyu3169@gmail.com>
2019-01-04 14:07:48 -0500

    storage class

--- app.js
+++ app.js
@@ -68,6 +68,33 @@ class UI {
 }
 
 // Store Class: Handle Storage
+class Store {
+  static getBooks() {
+    let books;
+
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const book = Store.getBooks();
+    books = books.filter(book => (book.isbn = isbn));
+    localStorage.setItem("books", JSOON.stringify(books));
+  }
+}
 
 // Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
