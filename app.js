3ae3b191fd577a96c819c4be30d468a897d5784a
Shiyu Wang <shiyu3169@gmail.com>
2019-01-04 11:09:42 -0500

    classes

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // Book Class: Reprents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
 
 // UI Class: Handle UI Tasks
 
