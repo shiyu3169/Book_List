d718a21f08a77968f055afee871122a4fd484572
Shiyu Wang <shiyu3169@gmail.com>
2019-01-04 13:50:03 -0500

    delete book

--- app.js
+++ app.js
@@ -37,10 +37,16 @@ class UI {
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
-      <td><a href="# class="btn btn-danger btn-sm delete">X</a></td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
 
     list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
   }
 
   static clearFields() {
@@ -75,3 +81,6 @@ document.querySelector("#book-form").addEventListener("submit", e => {
 });
 
 // Event: Remove a Book
+document.querySelector("#book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
+});
